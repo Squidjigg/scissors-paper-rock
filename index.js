7b5121d22ca4b7f3649ce47b6985db39c9481fab
Squidjigg <squidjigg80@outlook.com>
2022-08-03 22:50:31 +1000

    Add audio files, initial implementation of sounds

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
 var playerNum1 = Math.floor(Math.random() * 3) + 1;
 var playerNum2 = Math.floor(Math.random() * 3) + 1;
 
+// var playerAudio1 = new Audio("sounds/horn-stabs-entrance.mp3");
+// var playerAudio2 = new Audio("sounds/success-fanfare-trumpets.mp3");
+// var playerAudio3 = new Audio("sounds/surprise-sound-effect.mp3");
+
 // Random number assignment: 1-scissors, 2-paper, 3-rock
 
 document.querySelector(".img1").setAttribute("src", "images/player1_" + playerNum1 + ".png");
 document.querySelector(".img2").setAttribute("src", "images/player2_" + playerNum2 + ".png");
 
-if(playerNum1 === 3 && playerNum2 === 1 ) {
+function playerWinSound1() {
+    var playerAudio1 = new Audio("sounds/horn-stabs-entrance.mp3");
+    playerAudio1.play();
+}
+
+function playerWinSound2() {
+    var playerAudio2 = new Audio("sounds/success-fanfare-trumpets.mp3");
+    playerAudio2.play();
+}
+
+function playerDraw() {
+    var playerAudio3 = new Audio("sounds/surprise-sound-effect.mp3");
+    playerAudio3.play();
+}
+
+if (playerNum1 === 3 && playerNum2 === 1) {
     // rock beats scissors
     // player 1 wins 
-    document.querySelector("h1").textContent = "🏆 Player 1 Wins!"
+    document.querySelector("h1").textContent = "🏆 Player 1 Wins!";
+    playerWinSound1();
 }
-else if(playerNum1 === 3 && playerNum2 === 2) {
+else if (playerNum1 === 3 && playerNum2 === 2) {
     // paper beats rock
     // player 2 wins
-    document.querySelector("h1").textContent = "Player 2 Wins! 🏆"
+    document.querySelector("h1").textContent = "Player 2 Wins! 🏆";
+    playerWinSound2();
 }
-else if(playerNum1 === 2 && playerNum2 == 1) {
+else if (playerNum1 === 2 && playerNum2 == 1) {
     // scissors beats paper
     // player 2 wins
-    document.querySelector("h1").textContent = "Player 2 Wins! 🏆"
+    document.querySelector("h1").textContent = "Player 2 Wins! 🏆";
+    playerWinSound2();
 }
-else if(playerNum1 === 1 && playerNum2 === 2) {
+else if (playerNum1 === 1 && playerNum2 === 2) {
     // scissors beats paper
     // player 1 wins
-    document.querySelector("h1").textContent = "🏆 Player 1 Wins!"
+    document.querySelector("h1").textContent = "🏆 Player 1 Wins!";
+    playerWinSound1();
 }
-else if(playerNum1 === 1 && playerNum2 === 3) {
+else if (playerNum1 === 1 && playerNum2 === 3) {
     // rock beats scissors
     // player 2 wins
-    document.querySelector("h1").textContent = "Player 2 Wins! 🏆"
+    document.querySelector("h1").textContent = "Player 2 Wins! 🏆";
+    playerWinSound2();
 }
-else if(playerNum1 === 2 && playerNum2 === 3) {
+else if (playerNum1 === 2 && playerNum2 === 3) {
     // paper beats rock
     // player 1 wins
-    document.querySelector("h1").textContent = "🏆 Player 1 Wins!"
+    document.querySelector("h1").textContent = "🏆 Player 1 Wins!";
+    playerWinSound1();
 }
 else if (playerNum1 === playerNum2) {
     // draw
-    document.querySelector("h1").textContent = "😲 Draw! 😲"
+    document.querySelector("h1").textContent = "😲 Draw! 😲";
+    playerDraw();
 }
